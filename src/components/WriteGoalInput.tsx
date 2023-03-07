7026ecab0602f7866ba6a73d6dd8ec6a0db4f370
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-07 10:57:03 +0100

    Made component of user input field

---
+++ src/components/WriteGoalInput.tsx
@@ -0,0 +1,20 @@
+import { Flex, Input } from '@chakra-ui/react'
+
+export default function WriteGoalInput() {
+  return (
+    <Flex mb="15px" borderRadius="md" justifyContent="center">
+      <Input
+        placeholder="Write Your Goal"
+        bg="white"
+        size="lg"
+        w="35vw"
+        minW="500px"
+        p="1.5rem"
+        _placeholder={{ color: 'gray.400' }}
+        _focus={{ borderColor: 'teal.400' }}
+        onChange={(e) => console.log(e.target.value)}
+        onKeyDown={(e) => console.log(e.key)}
+      />
+    </Flex>
+  )
+}
