d4e69a2d2f163990f285ecdf2a4934e6972b0fec
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-07 16:13:40 +0100

    Active header is now based on url path

--- src/components/Headers/PageContentHeader.tsx
+++ src/components/Headers/PageContentHeader.tsx
@@ -4,7 +4,7 @@ import PageHeaderToggle from '../PageToggle/PageHeaderToggle'
 export default function PageContentHeader() {
   return (
     <Flex align="baseline" mb="15px">
-      <PageHeaderToggle active={true} />
+      <PageHeaderToggle />
       <Spacer />
       <Button bg="teal.400" textColor="white" rounded="3xl">
         Create a new goal
--- src/components/PageToggle/PageHeaderToggle.tsx
+++ src/components/PageToggle/PageHeaderToggle.tsx
@@ -1,33 +1,37 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Flex, Link, Text } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { useEffect, useState } from 'react'
 
-type Props = {
-  active: boolean
-}
+const PageHeaderToggle: React.FC = () => {
+  const [toggleActive, setToggleActive] = useState(false)
 
-const PageHeaderToggle: React.FC<Props> = ({ active }) => {
-  const [toggleActive, setToggleActive] = useState(active)
+  useEffect(() => {
+    if (window.location.pathname === '/') setToggleActive(true)
+    else setToggleActive(false)
+  }, [window.location.pathname])
 
   return (
     <Flex gap={25}>
-      <Text
-        fontSize={30}
-        fontWeight={toggleActive ? 'bold' : 'normal'}
-        color={toggleActive ? 'black' : 'silver'}
-        cursor="pointer"
-        onClick={() => setToggleActive(true)}
-      >
-        Active Goals
-      </Text>
-      <Text
-        fontSize={30}
-        color={!toggleActive ? 'black' : 'silver'}
-        fontWeight={!toggleActive ? 'bold' : 'normal'}
-        cursor="pointer"
-        onClick={() => setToggleActive(false)}
-      >
-        All Goals
-      </Text>
+      <Link as={NextLink} href="/" _hover={{ textDecoration: 'none' }}>
+        <Text
+          fontSize={30}
+          fontWeight={toggleActive ? 'bold' : 'normal'}
+          color={toggleActive ? 'black' : 'silver'}
+          cursor="pointer"
+        >
+          Active Goals
+        </Text>
+      </Link>
+      <Link as={NextLink} href="/goals" _hover={{ textDecoration: 'none' }}>
+        <Text
+          fontSize={30}
+          color={!toggleActive ? 'black' : 'silver'}
+          fontWeight={!toggleActive ? 'bold' : 'normal'}
+          cursor="pointer"
+        >
+          All Goals
+        </Text>
+      </Link>
     </Flex>
   )
 }
