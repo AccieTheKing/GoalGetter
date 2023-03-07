87563677f33498addf183c8b4a06c465f0520a70
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-07 14:44:53 +0100

    Conditional rendering of created goals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GoalCard from '@/components/Cards/GoalCard'
 import PageContentHeader from '@/components/Headers/PageContentHeader'
 import WriteGoalInput from '@/components/WriteGoalInput'
 import useMockGoals from '@/hooks/useMockGoals'
-import { Box, SimpleGrid } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
 
 export default function Home() {
   const [mockGoals, setMockGoals] = useMockGoals()
@@ -14,11 +14,19 @@ export default function Home() {
         <PageContentHeader />
         <Box p="10px" bg="gray.50">
           <WriteGoalInput onCreate={setMockGoals} />
-          <SimpleGrid gap={5} columns={{ sm: 2, md: 4 }}>
-            {mockGoals?.map((goal) => (
-              <GoalCard key={goal.title} {...goal} />
-            ))}
-          </SimpleGrid>
+          {mockGoals && mockGoals.length > 0 ? (
+            <SimpleGrid gap={5} columns={{ sm: 2, md: 4 }}>
+              {mockGoals.map((goal) => (
+                <GoalCard key={goal.title} {...goal} />
+              ))}
+            </SimpleGrid>
+          ) : (
+            <Flex h="70vh" alignItems="center" justifyContent="center">
+              <Text fontSize="2rem" fontWeight="extrabold">
+                No Goals Created Yet😅
+              </Text>
+            </Flex>
+          )}
         </Box>
       </Box>
     </main>
