4ed89c08d7e36aa72d8c9f58f8e8b3e164e984c9
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-07 14:42:35 +0100

    Updated Goal creation form
    - A message being shows when the selected deadline is not a future date

--- src/components/WriteGoalInput.tsx
+++ src/components/WriteGoalInput.tsx
@@ -1,6 +1,15 @@
 import { Goal } from '@/types'
-import { Collapse, Flex, Input, Textarea } from '@chakra-ui/react'
-import { createRef, Dispatch, SetStateAction, useRef, useState } from 'react'
+import {
+  Button,
+  Collapse,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react'
+import { Dispatch, SetStateAction, useRef, useState } from 'react'
 import { GoalStatus } from './StatusBadge'
 
 type Props = {
@@ -16,7 +25,29 @@ export default function WriteGoalInput({ onCreate }: Props) {
   const descRef = useRef<HTMLTextAreaElement | null>(null)
 
   const isTitleFilledIn = title.trim().length > 0
-  const isValidGoal = isTitleFilledIn && description.trim().length > 0
+  const isDescFilledIn = description.trim().length > 0
+  const isDateValid = date.trim().length > 0 && new Date(date) > new Date()
+  const isValidGoal = isTitleFilledIn && isDescFilledIn && isDateValid
+
+  const onCreateGoal = () => {
+    if (isValidGoal) {
+      onCreate((prev) => [
+        ...prev,
+        {
+          title,
+          description,
+          id: Date.now().toString(),
+          status: GoalStatus.NOT_COMPLETED,
+          createdAt: new Date().toLocaleDateString(new Intl.Locale('nl-NL')),
+          completeBefore: new Date(date).toLocaleDateString(
+            new Intl.Locale('nl-NL')
+          ),
+        },
+      ])
+      setTitle('')
+      setDescription('')
+    }
+  }
 
   const onCreateOnEnterKey = (
     e: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -25,31 +56,15 @@ export default function WriteGoalInput({ onCreate }: Props) {
       descRef.current?.focus()
     }
 
-    if (e.key === 'Enter' && isValidGoal) {
-      onCreate((prev) => [
-        ...prev,
-        {
-          title,
-          description,
-          id: Date.now().toString(),
-          status: GoalStatus.NOT_COMPLETED,
-        },
-      ])
-      setTitle('')
-      setDescription('')
-    } else {
-      descRef.current?.focus()
-    }
+    if (e.key === 'Enter' && isValidGoal) onCreateGoal()
   }
 
   const onHandleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value)
-    console.log(e.target.value)
   }
 
   const onHandleDescChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setDescription(e.target.value)
-    console.log(e.target.value)
   }
 
   const inputStyles = {
@@ -71,23 +86,63 @@ export default function WriteGoalInput({ onCreate }: Props) {
       alignItems="center"
       gap={5}
     >
+      <Collapse in={isTitleFilledIn}>
+        <FormLabel fontWeight="extrabold" mb={0}>
+          New Goal
+        </FormLabel>
+      </Collapse>
       <Input
         value={title}
         {...inputStyles}
+        borderColor={isTitleFilledIn ? 'green' : ''}
         onChange={onHandleTitleChange}
         onKeyDown={onCreateOnEnterKey}
         placeholder="Write Your Goal"
       />
       <Collapse in={isTitleFilledIn} animateOpacity>
-        <Textarea
-          ref={descRef}
-          value={description}
-          {...inputStyles}
-          minH="170px"
-          onChange={onHandleDescChange}
-          onKeyDown={onCreateOnEnterKey}
-          placeholder="Write a description for your goal."
-        />
+        <Flex direction="column" gap={5}>
+          <FormControl>
+            <FormLabel>Description</FormLabel>
+            <Textarea
+              ref={descRef}
+              value={description}
+              cols={30}
+              rows={5}
+              {...inputStyles}
+              borderColor={isDescFilledIn ? 'green' : ''}
+              onChange={onHandleDescChange}
+              onKeyDown={onCreateOnEnterKey}
+              placeholder="Write a description for your goal."
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Complete before:</FormLabel>
+            <Input
+              placeholder="Select Date and Time"
+              size="md"
+              value={date}
+              {...inputStyles}
+              borderColor={
+                isDateValid
+                  ? 'green'
+                  : date.length > 0 && !isDateValid
+                  ? 'red'
+                  : ''
+              }
+              type="date"
+              onChange={(e) => setDate(e.target.value)}
+            />
+            {date.length > 0 && !isDateValid && (
+              <FormHelperText>
+                Please select a date in the future
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <Button size="lg" colorScheme="teal" onClick={onCreateGoal}>
+            Create Goal
+          </Button>
+        </Flex>
       </Collapse>
     </Flex>
   )
