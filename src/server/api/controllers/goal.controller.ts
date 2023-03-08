f4ba4ee453fe5a98854dded72bcd26e3bd2d782b
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-08 12:03:13 +0100

    Created controllers and services for goals
    - the controllers will invoke the corresponding service
    - the services file consists over functions that will communicate with the database layer

---
+++ src/server/api/controllers/goal.controller.ts
@@ -0,0 +1,88 @@
+import {
+  CreateGoalInput,
+  FilterQueryInput,
+  ParamsInput,
+  UpdateGoalInput,
+} from '@/server/schema/goal.schema'
+import { Context } from '../createContext'
+import {
+  createGoal,
+  deleteGoal,
+  findAllPosts,
+  findUniqueGoal,
+  updateGoal,
+} from '../services/goal.services'
+
+export const createGoalHandler = async ({
+  input,
+  ctx,
+}: {
+  input: CreateGoalInput
+  ctx: Context
+}) => {
+  try {
+    const USER_ID = '6408634265f1fb962dd039d1' // is going to be replaced with the user id from the context
+    const createdGoal = await createGoal({
+      title: input.title,
+      description: input.description,
+      completeBefore: input.completeBefore,
+      user: { connect: { id: USER_ID } },
+    })
+    return createdGoal
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getGoalHandler = async ({
+  paramsInput,
+}: {
+  paramsInput: ParamsInput
+}) => {
+  try {
+    const goal = await findUniqueGoal({ id: paramsInput.goalId })
+    return goal
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getGoalsHandler = async ({
+  filterQuery,
+}: {
+  filterQuery: FilterQueryInput
+}) => {
+  try {
+    const goals = await findAllPosts(filterQuery.page, filterQuery.limit)
+    return goals
+  } catch (error) {
+    throw error
+  }
+}
+
+export const updateGoalHandler = async ({
+  paramsInput,
+  input,
+}: {
+  paramsInput: ParamsInput
+  input: UpdateGoalInput
+}) => {
+  try {
+    const goal = await updateGoal({ id: paramsInput.goalId }, input.body)
+    return goal
+  } catch (error) {
+    throw error
+  }
+}
+
+export const deleteGoalHandler = async ({
+  paramsInput,
+}: {
+  paramsInput: ParamsInput
+}) => {
+  try {
+    await deleteGoal({ id: paramsInput.goalId })
+  } catch (error) {
+    throw error
+  }
+}
---
+++ src/server/api/services/goal.services.ts
@@ -0,0 +1,60 @@
+import { prisma } from '@/server/db'
+import { Goal, Prisma } from '@prisma/client'
+
+/**
+ * Service functions
+ *
+ * - Services are used to interact with the database, trough controllers.
+ * Instead of using the controllers directly to interact with the databse
+ *
+ * @param input
+ * @returns
+ */
+
+export const createGoal = async (input: Prisma.GoalCreateInput) => {
+  return (await prisma.goal.create({
+    data: input,
+  })) as Goal
+}
+
+export const findGoal = async (
+  where: Partial<Prisma.GoalWhereInput>,
+  select?: Prisma.GoalSelect
+) => {
+  return (await prisma.goal.findFirst({
+    where,
+    select,
+  })) as Goal
+}
+
+export const findUniqueGoal = async (
+  where: Prisma.GoalWhereUniqueInput,
+  select?: Prisma.GoalSelect
+) => {
+  return (await prisma.goal.findUnique({
+    where,
+    select,
+  })) as Goal
+}
+
+export const findAllPosts = async (page: number, limit: number) => {
+  const take = limit || 10
+  const skip = (page - 1) * limit
+  return (await prisma.goal.findMany({
+    include: { user: true },
+    skip,
+    take,
+  })) as Goal[]
+}
+
+export const updateGoal = async (
+  where: Partial<Prisma.GoalWhereUniqueInput>,
+  data: Prisma.GoalUpdateInput,
+  select?: Prisma.GoalSelect
+) => {
+  return (await prisma.goal.update({ where, data, select })) as Goal
+}
+
+export const deleteGoal = async (where: Prisma.GoalWhereUniqueInput) => {
+  return await prisma.goal.delete({ where })
+}
