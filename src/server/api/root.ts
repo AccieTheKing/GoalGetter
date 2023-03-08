4af0ceb16349970587a2b2b3cd7fdd1c68c9fc51
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-08 11:59:55 +0100

    Renamed exampleRouter to goalsRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from '@/server/api/trpc'
-import { exampleRouter } from '@/server/api/routers/example'
+import { goalsRouter } from '@/server/api/routers/goals'
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from '@/server/api/routers/example'
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  goalsRouter,
 })
 
 // export type definition of API
