a947597639de135ba53869d15f0c2efe3c5f6d7e
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-08 10:43:07 +0100

    Created schema with zod validation for trcp calls

---
+++ src/server/schema/goal.schema.ts
@@ -0,0 +1,32 @@
+import z, { TypeOf } from 'zod'
+
+export const createGoalSchema = z.object({
+  title: z.string({
+    required_error: 'Title of id is required',
+  }),
+  description: z.string({
+    required_error: 'Description of id is required',
+  }),
+  completeBerore: z.date({
+    required_error: 'The date that the goal should be completed by is required',
+  }),
+})
+
+export const params = z.object({
+  goalId: z.string(),
+})
+
+export const updateGoalSchema = z.object({
+  params,
+  body: createGoalSchema.partial(),
+})
+
+export const filterQuery = z.object({
+  limit: z.number().default(1),
+  page: z.number().default(10),
+})
+
+export type CreateGoalInput = TypeOf<typeof createGoalSchema>
+export type UpdateGoalInput = TypeOf<typeof updateGoalSchema>
+export type ParamsInput = TypeOf<typeof params>
+export type FilterQueryInput = TypeOf<typeof filterQuery>
