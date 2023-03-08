97a1426c47436e3cc44253f7fdb5054c97430d0b
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-08 17:10:20 +0100

    Renamed mockgoals to allNewGoals
    - This hook was previously used to make a mock list of goals

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,17 @@ import useGoals from '@/hooks/useGoals'
 import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
 
 export default function Home() {
-  const [mockGoals, setMockGoals] = useGoals({ withStatus: 'NEW' })
+  const [allNewGoals, setNewGoal] = useGoals({ withStatus: 'NEW' })
 
   return (
     <main>
       <Box p="15px" m="10px">
         <PageContentHeader />
         <Box p="10px" bg="gray.50">
-          <WriteGoalInput onCreate={setMockGoals} />
-          {mockGoals && mockGoals.length > 0 ? (
+          <WriteGoalInput onCreate={setNewGoal} />
+          {allNewGoals && allNewGoals.length > 0 ? (
             <SimpleGrid gap={5} columns={{ base: 1, md: 2, lg: 3, xl: 4 }}>
-              {mockGoals.map((goal) => (
+              {allNewGoals.map((goal) => (
                 <GoalCard key={goal.title} {...goal} />
               ))}
             </SimpleGrid>
