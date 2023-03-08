c1b56c7ab844a0650aa3c4c4578c204671d7e972
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-08 13:26:28 +0100

    Added all endpoints to goalsRouter

--- src/server/api/routers/goals.ts
+++ src/server/api/routers/goals.ts
@@ -1,15 +1,36 @@
-import { z } from 'zod'
-
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
+import {
+  createGoalSchema,
+  filterQuery,
+  goalParams,
+  updateGoalSchema,
+} from '@/server/schema/goal.schema'
+import {
+  createGoalHandler,
+  deleteGoalHandler,
+  getGoalHandler,
+  getGoalsHandler,
+  updateGoalHandler,
+} from '../controllers/goal.controller'
 
 export const goalsRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      }
-    }),
+  createGoal: publicProcedure
+    .input(createGoalSchema)
+    .mutation(({ input, ctx }) => createGoalHandler({ input })),
+  updateGoal: publicProcedure
+    .input(updateGoalSchema)
+    .mutation(({ input }) =>
+      updateGoalHandler({ input, paramsInput: input.params })
+    ),
+  deleteGoal: publicProcedure
+    .input(goalParams)
+    .mutation(({ input }) => deleteGoalHandler({ paramsInput: input })),
+  getPost: publicProcedure
+    .input(goalParams)
+    .query(({ input }) => getGoalHandler({ paramsInput: input })),
+  getPosts: publicProcedure
+    .input(filterQuery)
+    .query(({ input }) => getGoalsHandler({ filterQuery: input })),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.goal.findMany()
   }),
