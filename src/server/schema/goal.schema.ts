1f96f7df708b228b058fa49a3bb2d2e7e5252c3c
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-09 10:27:00 +0100

    Created goalStatus enum with zod

--- src/server/schema/goal.schema.ts
+++ src/server/schema/goal.schema.ts
@@ -1,6 +1,13 @@
 import { GoalStatus } from '@prisma/client'
 import z, { TypeOf } from 'zod'
 
+const goalStatus = z.enum([
+  GoalStatus.COMPLETED,
+  GoalStatus.IN_REVIEW,
+  GoalStatus.NEW,
+  GoalStatus.NOT_COMPLETED,
+])
+
 export const createGoalSchema = z.object({
   title: z.string({
     required_error: 'Title of id is required',
@@ -8,6 +15,7 @@ export const createGoalSchema = z.object({
   description: z.string({
     required_error: 'Description of id is required',
   }),
+  status: goalStatus.optional(),
   completeBefore: z.date({
     required_error: 'The date that the goal should be completed by is required',
   }),
@@ -25,14 +33,7 @@ export const updateGoalSchema = z.object({
 export const filterQuery = z.object({
   limit: z.number().default(1),
   page: z.number().default(10),
-  status: z
-    .enum([
-      GoalStatus.COMPLETED,
-      GoalStatus.IN_REVIEW,
-      GoalStatus.NEW,
-      GoalStatus.NOT_COMPLETED,
-    ])
-    .optional(),
+  status: goalStatus.optional(),
 })
 
 export type CreateGoalInput = TypeOf<typeof createGoalSchema>
