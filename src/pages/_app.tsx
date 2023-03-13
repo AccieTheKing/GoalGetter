6256eddfc2e2d58420d691ab7fc5c2f8f62e3001
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2023-03-13 14:38:48 +0100

    Added react-query devtools
    - Devtools added in order to inspect requests with react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { type AppType } from 'next/app'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { api } from '@/utils/api'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
