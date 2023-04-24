3981b4a7b2d4656741ddcc0843351735b1a2240e
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-04-24 12:27:18 +0200

    Wrapped application with a session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { type AppType } from 'next/app'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { api } from '@/utils/api'
+import { SessionProvider } from 'next-auth/react'
+import { Session } from 'next-auth'
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType<{ session: Session }> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </SessionProvider>
     </ChakraProvider>
   )
 }
