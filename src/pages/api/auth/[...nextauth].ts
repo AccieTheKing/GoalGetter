a2dd1c3de6ff732296324cca141dcdb1ce20eb39
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-04-24 12:22:29 +0200

    Added Google authentication provider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth, { AuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    signIn: ({ account, profile }) => {
+      if (account?.provider === 'google') {
+        return !!(profile?.email && profile.email.endsWith('@oberon.nl'))
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  },
+}
+
+export default NextAuth(authOptions)
