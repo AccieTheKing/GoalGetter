e5079fb67424a573706559dd5d7c998754262b8c
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-05-08 14:11:21 +0200

    Cleaned up _app.tsx
    - Destructing props for cleaner code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { type AppType } from 'next/app'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { api } from '@/utils/api'
-import { SessionProvider } from 'next-auth/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { type AppType } from 'next/app'
 
-const MyApp: AppType<{ session: Session }> = ({ Component, pageProps }) => {
+const MyApp: AppType<{ session: Session }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
     <ChakraProvider>
-      <SessionProvider session={pageProps.session}>
+      <SessionProvider session={session}>
         <ReactQueryDevtools initialIsOpen={false} />
         <Component {...pageProps} />
       </SessionProvider>
