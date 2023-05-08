39fd4cdcd322cc70277d90f05cdf02b787a6ecf5
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-05-08 15:22:58 +0200

    Passing session trough getServersideProps
    - Need to test this but using this instead of the useSession hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,72 @@
 import GoalCard from '@/components/Cards/GoalCard'
-import PageContentHeader from '@/components/Headers/PageContentHeader'
+import Layout from '@/components/Layout'
 import WriteGoalInput from '@/components/WriteGoalInput'
 import useGoals from '@/hooks/useGoals'
 import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from 'next'
+import { getServerSession, Session } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]'
 
-export default function Home() {
+type Data = {
+  session: Session
+}
+
+export default function Home({
+  session,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [allNewGoals, setNewGoal] = useGoals({ withStatus: 'NEW' })
 
   return (
-    <main>
-      <Box p="15px" m="10px">
-        <PageContentHeader />
-        <Box p="10px" bg="gray.50">
-          <WriteGoalInput onCreate={setNewGoal} />
-          {allNewGoals && allNewGoals.length > 0 ? (
-            <SimpleGrid
-              gap={5}
-              columns={{ base: 1, md: 2, lg: 3, xl: 4, '2xl': 5 }}
-            >
-              {allNewGoals.map((goal) => (
-                <GoalCard key={goal.title} {...goal} />
-              ))}
-            </SimpleGrid>
-          ) : (
-            <Flex h="70vh" alignItems="center" justifyContent="center">
-              <Text fontSize="2rem" fontWeight="extrabold">
-                No Goals Created Yet😅
-              </Text>
-            </Flex>
-          )}
-        </Box>
+    <Layout data={session}>
+      <Box p="10px" bg="gray.50">
+        <WriteGoalInput onCreate={setNewGoal} />
+        {allNewGoals && allNewGoals.length > 0 ? (
+          <SimpleGrid
+            gap={5}
+            columns={{ base: 1, md: 2, lg: 3, xl: 4, '2xl': 4 }}
+          >
+            {allNewGoals.map((goal) => (
+              <GoalCard key={goal.title} {...goal} />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <Flex h="70vh" alignItems="center" justifyContent="center">
+            <Text fontSize="2rem" fontWeight="extrabold">
+              No Goals Created Yet😅
+            </Text>
+          </Flex>
+        )}
       </Box>
-    </main>
+    </Layout>
   )
+}
+
+/**
+ * This function is used to check if the user is authenticated by getting the session on server
+ * and check its existence, if there is no valid session it will redirect to the login page.
+ *
+ * @param context
+ * @returns
+ */
+export const getServerSideProps: GetServerSideProps<Data> = async (
+  context: GetServerSidePropsContext
+) => {
+  const session = await getServerSession(context.req, context.res, authOptions)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { session },
+  }
 }
