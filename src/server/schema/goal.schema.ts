c47f460d9bc76c3dde5b1f2669eadbd9f4a110b9
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-05-22 15:55:06 +0200

    Added some comments to explain pieces of code

--- src/server/schema/goal.schema.ts
+++ src/server/schema/goal.schema.ts
@@ -8,12 +8,16 @@ const goalStatus = z.enum([
   GoalStatus.NOT_COMPLETED,
 ])
 
+/**
+ * This is the base schema for creating a goal.
+ * These are the required fields for creating a goal.
+ */
 export const createGoalSchema = z.object({
   title: z.string({
-    required_error: 'Title of id is required',
+    required_error: 'Title of goal is required',
   }),
   description: z.string({
-    required_error: 'Description of id is required',
+    required_error: 'Description of goal is required',
   }),
   status: goalStatus.optional(),
   completeBefore: z.date({
@@ -21,21 +25,36 @@ export const createGoalSchema = z.object({
   }),
 })
 
+/**
+ * When querying for a goal, the only required field is the goalId.
+ */
 export const goalParams = z.object({
   goalId: z.string(),
 })
 
+/**
+ * When updating a goal, the params only need goal params and the body containing new values
+ * for updating a goal.
+ */
 export const updateGoalSchema = z.object({
   params: goalParams,
   body: createGoalSchema.partial(),
 })
 
+/**
+ * This is the filter query for getting goals.
+ */
 export const filterQuery = z.object({
   limit: z.number().default(1),
   page: z.number().default(10),
   status: goalStatus.optional(),
 })
 
+/**
+ * These are the types for the schemas above.
+ * These are used in the controllers.
+ *
+ * */
 export type CreateGoalInput = TypeOf<typeof createGoalSchema>
 export type UpdateGoalInput = TypeOf<typeof updateGoalSchema>
 export type ParamsInput = TypeOf<typeof goalParams>
