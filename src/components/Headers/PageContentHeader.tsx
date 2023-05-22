e9ad422f1de4c96182eeb50410e34d6060ef307d
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-05-22 11:42:53 +0200

    Created dropdown menu
    - Created a dropdown menu that opens when clicking on the profile picture

--- src/components/Headers/PageContentHeader.tsx
+++ src/components/Headers/PageContentHeader.tsx
@@ -1,14 +1,55 @@
-import { Flex, Spacer, Button } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+} from '@chakra-ui/react'
+import { DefaultSession } from 'next-auth'
+import { signOut } from 'next-auth/react'
 import PageHeaderToggle from '../PageToggle/PageHeaderToggle'
 
-export default function PageContentHeader() {
+type Props = {
+  user: DefaultSession['user']
+}
+
+export default function PageContentHeader({ user }: Props) {
+  const onLogout = () => signOut()
+
+  if (!user) {
+    return <Box></Box>
+  }
+
   return (
-    <Flex align="baseline" mb="15px">
-      <PageHeaderToggle />
-      <Spacer />
-      <Button bg="teal.400" textColor="white" rounded="3xl">
-        Create a new goal
-      </Button>
-    </Flex>
+    <Menu>
+      <Flex
+        align="baseline"
+        mb="15px"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <PageHeaderToggle />
+        <Spacer />
+        <Button bg="teal.400" textColor="white" rounded="3xl">
+          Create a new goal
+        </Button>
+        <MenuButton>
+          <Image
+            src={user.image ?? '/images/placeholder.png'}
+            rounded="full"
+            width={50}
+            height={50}
+            ml="10px"
+          />
+        </MenuButton>
+        <MenuList p={0}>
+          <MenuItem onClick={onLogout}>Logout</MenuItem>
+        </MenuList>
+      </Flex>
+    </Menu>
   )
 }
