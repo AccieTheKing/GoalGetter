402f7551d37d942ec6a3e99a329c6bf588dfc1c7
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-05-22 09:56:31 +0200

    Removed custom signin and signout pages from config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,10 +19,6 @@ export const authOptions: AuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET as string,
-  pages: {
-    signIn: '/auth',
-    signOut: '/auth',
-  },
 }
 
 export default NextAuth(authOptions)
