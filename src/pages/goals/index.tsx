a79bfc94139790d198fd998bda3b8a5fec7ff8d1
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-05-22 11:30:46 +0200

    Session check on server and redirect when not auth
    - Redirect if the user is not authenticated

--- src/pages/goals/index.tsx
+++ src/pages/goals/index.tsx
@@ -1,23 +1,45 @@
 import GoalCard from '@/components/Cards/GoalCard'
-import PageHeaderToggle from '@/components/PageToggle/PageHeaderToggle'
+import Layout from '@/components/Layout'
 import useGoals from '@/hooks/useGoals'
-import { Box, SimpleGrid } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
+import { SimpleGrid } from '@chakra-ui/react'
+import { GetServerSidePropsContext } from 'next'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]'
 
 export default function GoalsPage() {
   const [allGoals, setAllGoals] = useGoals()
 
   return (
-    <Box p="15px" m="10px">
-      <PageHeaderToggle />
-      <Box p="15px" m="10px">
-        Goals
-      </Box>
-      <SimpleGrid gap={5} columns={{ base: 1, md: 2, lg: 3, xl: 4, '2xl': 5 }}>
+    <Layout>
+      <SimpleGrid gap={5} columns={{ base: 1, md: 2, lg: 3, xl: 4, '2xl': 4 }}>
         {allGoals.map((goal) => (
           <GoalCard key={goal.title} {...goal} />
         ))}
       </SimpleGrid>
-    </Box>
+    </Layout>
   )
+}
+
+/**
+ * This function is used to check if the user is authenticated by getting the session on server
+ * and check its existence, if there is no valid session it will redirect to the login page.
+ *
+ * @param context
+ * @returns
+ */
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { session },
+  }
 }
