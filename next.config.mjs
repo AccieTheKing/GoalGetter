b60f9dd74119379bca258fbaf32054224ea67147
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-06-05 12:09:26 +0200

    Fixed the profile image domain issue

--- next.config.mjs
+++ next.config.mjs
@@ -23,10 +23,7 @@ const config = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '*',
+        hostname: '*.googleusercontent.com',
       },
     ],
   },
