23461260987f9bee47625e790e4d51b134204407
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-06-05 11:45:55 +0200

    Added fallback profile image if user has none
    - Added fallback image for when the user has no profile image set

--- src/components/Headers/PageContentHeader.tsx
+++ src/components/Headers/PageContentHeader.tsx
@@ -39,7 +39,7 @@ export default function PageContentHeader({ user }: Props) {
         </Button>
         <MenuButton>
           <Image
-            src={user.image ?? '/images/placeholder.png'}
+            src={user?.image ?? '/vercel.svg'}
             rounded="full"
             width={50}
             height={50}
