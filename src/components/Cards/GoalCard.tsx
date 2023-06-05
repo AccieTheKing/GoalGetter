7e0a51bed07c17e682217a579610652ac2792615
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-06-05 13:55:01 +0200

    Able to navigate to detailed page for a goal

--- src/components/Cards/GoalCard.tsx
+++ src/components/Cards/GoalCard.tsx
@@ -13,6 +13,7 @@ import {
   Text,
 } from '@chakra-ui/react'
 import { Goal } from '@prisma/client'
+import { useRouter } from 'next/router'
 import StatusBadge from '../StatusBadge'
 
 type Props = Goal
@@ -24,37 +25,49 @@ export default function GoalCard({
   status,
   completeBefore,
 }: Props) {
+  const router = useRouter()
+
+  const onNavigateToGoalDetails = () => {
+    router.push(`/goals/${encodeURIComponent(id)}`)
+  }
+
   return (
-    <Card minW="xs" maxW="sm">
-      <Stack divider={<Divider color="silver" />}>
-        <CardHeader>
-          <Heading size="md" m="0">
-            {title} <StatusBadge status={status} />
-          </Heading>
-        </CardHeader>
+    <>
+      <Card>
+        <Stack divider={<Divider color="silver" />}>
+          <CardHeader>
+            <Heading size="lg" m="0" textAlign="center" fontWeight="extrabold">
+              {title} <StatusBadge status={status} />
+            </Heading>
+          </CardHeader>
 
-        <CardBody overflowY="auto" maxH="500">
-          <Text fontSize="0.9em">{description}</Text>
-        </CardBody>
+          <CardBody overflowY="auto" maxH="500">
+            <Text fontSize="0.9em">{description}</Text>
+          </CardBody>
 
-        <CardFooter display="column" textAlign="center">
-          <ButtonGroup m="auto">
-            <Button colorScheme="teal" variant="outline">
-              See details
-            </Button>
-          </ButtonGroup>
-          <Stat mt="0.5rem">
-            <StatHelpText>
-              Deadline:
-              <Text display="inline" fontWeight="bold" marginLeft="2px">
-                {new Date(completeBefore).toLocaleDateString(
-                  new Intl.Locale('nl-NL')
-                )}
-              </Text>
-            </StatHelpText>
-          </Stat>
-        </CardFooter>
-      </Stack>
-    </Card>
+          <CardFooter display="column" textAlign="center">
+            <ButtonGroup m="auto">
+              <Button
+                colorScheme="blackAlpha"
+                variant="outline"
+                onClick={onNavigateToGoalDetails}
+              >
+                See details
+              </Button>
+            </ButtonGroup>
+            <Stat mt="0.5rem">
+              <StatHelpText fontWeight="thin">
+                Deadline:
+                <Text display="inline" fontWeight="bold" marginLeft="2px">
+                  {new Date(completeBefore).toLocaleDateString(
+                    new Intl.Locale('nl-NL')
+                  )}
+                </Text>
+              </StatHelpText>
+            </Stat>
+          </CardFooter>
+        </Stack>
+      </Card>
+    </>
   )
 }
