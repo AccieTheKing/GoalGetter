b042bc001530bc995259015db2c12a4bdcb1a321
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-06-05 17:29:07 +0200

    Adjusted the Session properties on a session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,12 +1,8 @@
 import { DefaultUser } from 'next-auth'
-
 declare module 'next-auth' {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
-    user: DefaultUser & {
+    user?: {
       id: string
-    }
+    } & DefaultSession['user']
   }
 }
