be4479f33aa8dc3f6261f3ec6b2afa8d85170e08
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-07-31 16:58:42 +0200

    router destructed  and comment message style added

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,19 +1,45 @@
 import { api } from '@/utils/api'
-import { Box, Button, Heading, Text, Textarea } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Textarea,
+} from '@chakra-ui/react'
 import { useSession } from 'next-auth/react'
-import { FC, useState } from 'react'
+import { FC, useCallback, useState } from 'react'
 
 interface Props {
   goalId: string
 }
 
 const CommentsBlock: FC<Props> = ({ goalId }) => {
-  const { notesRouter } = api.useContext()
+  const { useContext, notesRouter: router } = api
+  const { notesRouter } = useContext()
   const { data: session } = useSession()
-  const { data, isLoading } = api.notesRouter.getNotes.useQuery({ goalId })
-  const { mutate } = api.notesRouter.createNote.useMutation()
+  const { data, isLoading } = router.getNotes.useQuery({ goalId })
+  const { mutate } = router.createNote.useMutation()
   const [comment, setComment] = useState<string>('')
   const hasContent = !isLoading && data && data.length > 0
+
+  const messageBackground = {
+    [session?.user.id as string]: {
+      textBubbleColor: '#CECECE',
+      align: 'flex-end',
+    },
+    default: {
+      textBubbleColor: '#E2E8F0',
+      align: 'unset',
+    },
+  }
+
+  const getMessageBackground = useCallback((userId: string) => {
+    return userId === session?.user.id
+      ? messageBackground[userId]
+      : messageBackground.default
+  }, [])
 
   const postNewComment = () => {
     if (comment && comment.trim().length > 0) {
@@ -38,11 +64,44 @@ const CommentsBlock: FC<Props> = ({ goalId }) => {
   return (
     <Box>
       <Heading>Comments </Heading>
-      <Box my="1.2rem">
+      <Box my="1.2rem" maxH={350} overflowY="auto" p={5}>
         {hasContent ? (
-          data.map((el, i) => {
-            return <Text key={i}>{el.description}</Text>
-          })
+          data.map((el, i) => (
+            <Box key={i} mb={4} w="full">
+              <Flex
+                width="100%"
+                justifyContent={getMessageBackground(el.userId)?.align}
+              >
+                <Image
+                  src={session?.user?.image ?? '/vercel.svg'}
+                  rounded="full"
+                  width={10}
+                  height={10}
+                  mr="15px"
+                />
+                <Box>
+                  <Box
+                    bg={getMessageBackground(el.userId)?.textBubbleColor}
+                    p={3}
+                    rounded="md"
+                  >
+                    {el.description}
+                  </Box>
+
+                  <Text
+                    textAlign="right"
+                    fontSize="0.8rem"
+                    fontWeight={500}
+                    mr={1.5}
+                  >
+                    {new Date(el?.createdAt ?? '').toLocaleTimeString(
+                      new Intl.Locale('nl-NL')
+                    )}
+                  </Text>
+                </Box>
+              </Flex>
+            </Box>
+          ))
         ) : (
           <Text>No notes created :(</Text>
         )}
